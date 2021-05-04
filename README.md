4e904006dc5757f800afb6cafe4b90515780bc15
RyanShaw21 <>
2021-05-04 17:10:41 +0000

    Created areaAndPerimeter.go

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ICS2O-Unit2-02-Go-Basic_Math
+[![GitHub's Super Linter](https://github.com/Ryan-Shaw-2/ICS2O-Unit2-02-Go-Basic_Math/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/Ryan-Shaw-2/ICS2O-Unit2-02-Go-Basic_Math/actions)
---
+++ areaAndPerimeter.go
@@ -0,0 +1,17 @@
+// Created by: Ryan-Shaw-2
+// Created on: May 2021
+//
+// This program calculates area and perimeter
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// This function calculates area and perimeter
+	fmt.Println("The dimensions of a rectangle are: 5cm and 3cm")
+	fmt.Println("The area is: ", (5 * 3), "cm²")
+	fmt.Println("The perimeter is: ", (2 * (5 + 3)), "cm")
+}
